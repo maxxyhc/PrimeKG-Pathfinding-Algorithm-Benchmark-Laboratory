7196d7f6cb8970c401080bcd711dc44c1022495d
Viveka Dhanda <viveka@Mac.lan>
2026-03-08 05:10:31 -0700

    refactor: remove unnecessary test config files

--- tests/conftest.py
+++
@@ -1,7 +0,0 @@
-import sys
-import os
-
-# Add notebook directory to path
-project_root = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-notebook_path = os.path.join(project_root, 'notebook')
-sys.path.insert(0, notebook_path)
